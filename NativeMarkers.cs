52e64fc969d21b04019e89fb33586d5666c0147c
Anton Kruglyakov <antonk@unity3d.com>
2019-02-25 00:09:31 +0000

    Draft commit

---
+++ NativeMarkers.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+public class NativeProfiler
+{
+    [DllImport("__Internal")]
+    private static extern int BeginSample([MarshalAs(UnmanagedType.LPWStr)]string str);
+
+    [DllImport("__Internal")]
+    private static extern int EndSample();
+
+    [DllImport("__Internal")]
+    private static extern int Event([MarshalAs(UnmanagedType.LPWStr)]string str);
+}
